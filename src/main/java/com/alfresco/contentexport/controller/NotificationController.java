383f6100cd7af6f887e439c766f4865ed2c4b47d
vivek.sharma <vivek.sharma@vlinkinfo.com>
2026-05-19 10:59:56 +0530

    added content download api

--- src/main/java/com/alfresco/contentexport/controller/NotificationController.java
+++ src/main/java/com/alfresco/contentexport/controller/NotificationController.java
@@ -2,8 +2,12 @@ package com.alfresco.contentexport.controller;
 
 import com.alfresco.contentexport.client.AlfrescoClient;
 import com.alfresco.contentexport.dto.DocListResponse;
+import org.springframework.http.ContentDisposition;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -25,5 +29,25 @@ public class NotificationController {
         return ResponseEntity
                 .status(response.getStatusCode())
                 .body(response.getBody());
+    }
+
+    //http://localhost:8080/api/v1/alfresco/nodes/1a0b110f-1e09-4ca2-b367-fe25e4964a4e/content
+    @GetMapping("/download/{nodeId}")
+    public ResponseEntity<byte[]> downloadContent(@PathVariable String nodeId) {
+        ResponseEntity<byte[]> response = alfrescoClient.getNodeContent(nodeId);
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_OCTET_STREAM);
+        headers.setContentDisposition(
+                ContentDisposition.attachment()
+                        .filename(nodeId)
+                        .build()
+        );
+
+        return new ResponseEntity<>(
+                response.getBody(),
+                headers,
+                response.getStatusCode()
+        );
     }
 }
